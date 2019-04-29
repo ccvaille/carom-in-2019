b6a62cc516a1eb8023415c1e0fa0d0db2bf5005d
coolfe <coolfe36@gmail.com>
2019-04-29 19:38:55 +0800

    feat: add note

---
+++ learn_typescript/hello.js
@@ -0,0 +1,21 @@
+function sayHello(person) {
+    return "Hello, " + person;
+}
+var user = 'ttt';
+console.log(sayHello(user));
+// let createdByNewBoolean: boolean = new Boolean(1);
+// console.log(createdByNewBoolean)
+var anyThing = 'hello';
+console.log(anyThing.myName);
+console.log(anyThing.myName.firstName);
+var tom = {
+    id: 1,
+    name: 'Tom',
+    gender: 'male'
+};
+var fibonacci = [1, 1, 3, 3, 5];
+console.log(fibonacci);
+var mySum = function (x, y) {
+    return x + y;
+};
+var xcatliu = [1, 'Xcat Liu'];
---
+++ learn_typescript/hello.ts
@@ -0,0 +1,42 @@
+function sayHello(person: string) {
+  return `Hello, ${person}`;
+}
+
+let user = 'ttt';
+console.log(sayHello(user));
+
+// let createdByNewBoolean: boolean = new Boolean(1);
+
+// console.log(createdByNewBoolean)
+
+let anyThing: any = 'hello';
+console.log(anyThing.myName);
+console.log(anyThing.myName.firstName);
+
+interface Person {
+  readonly id: number;
+  name: string;
+  age?: number;
+  [propName: string]: any;
+}
+
+let tom: Person = {
+  id: 1,
+  name: 'Tom',
+  gender: 'male'
+};
+
+// tom.id = 89757;
+
+interface NumberArray {
+  [index: number]: number;
+}
+let fibonacci: NumberArray = [1, 1, 3, 3, 5];
+console.log(fibonacci)
+
+let mySum: (a: number, b: number) => number = function (x: number, y: number): number {
+  return x + y;
+};
+
+let xcatliu: any[] = [1, 'Xcat Liu'];
+
